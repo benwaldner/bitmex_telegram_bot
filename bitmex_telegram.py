d6bd0b275b2d8298297505a320094e426e5fa3bc
khushalgelda <khushalgelda@gmail.com>
2021-03-11 14:07:22 +0530

    Update bitmex_telegram.py

--- bitmex_telegram.py
+++ bitmex_telegram.py
@@ -1,39 +1,68 @@
+import numpy
 import requests
-import pandas as pd
-import json
 from bitmex_websocket import BitMEXWebsocket
 from time import sleep
 
-pd.set_option('display.max_columns', None)
+
+# def get_funding_rates(base_ticker, period):
+#     url = f'https://www.bitmex.com/api/v1/funding'
+#     params = {'symbol': f'{base_ticker}',
+#               # 'count': f'{period}',
+#               'reverse': 'true'}
+#     response = requests.get(url, params=params)
+#     df = pd.DataFrame(json.loads(response.text))
+#     print(df)
 
 
-def funding_rate_sma(base_ticker, period):
-    url = f'https://www.bitmex.com/api/v1/funding'
-    params = {'symbol': f'{base_ticker}:nearest',
-              'count': f'{period}',
-              'reverse': 'true'}
-    response = requests.get(url, params=params)
-    df = pd.DataFrame(json.loads(response.text))
-    # print(json.loads(response.text))
-    print(df)
+def send_telegram(token, chat_id, msg):
+    url = f'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={msg}&parse_mode=html'
+    requests.get(url)
 
 
-def websocket_trigger(base_ticker, ticker, sma_period):
+def bitmex_funding_rate_trigger(ticker, sma_period, anomaly_threshold_percent, telegram_token, telegram_chat_id,
+                                bearish_bias_msg, bullish_bias_msg):
     ws = BitMEXWebsocket(endpoint=f'wss://www.bitmex.com/realtime', symbol=f'{ticker}')
-
+    rolling_data_set = []
+    i = 0
+    messaging = 0
     while ws.ws.sock.connected:
-        # print(ws.get_instrument())
         snapshot = ws.get_instrument()
         data = {'fundingRate': snapshot['fundingRate'],
                 'fundingTimestamp': snapshot['fundingTimestamp'],
                 'fundingInterval': snapshot['fundingInterval'],
-                'indicativeFundingRate': snapshot['indicativeFundingRate']}
-        funding_rate_sma(base_ticker, sma_period)
-        print(data)
-        sleep(2)
+                'indicativeFundingRate': snapshot['indicativeFundingRate'],
+                'symbol': snapshot['symbol']}
+
+        if i < sma_period:
+            rolling_data_set.append(data['indicativeFundingRate'])
+            i = i + 1
+        else:
+            diff = data['indicativeFundingRate'] - numpy.mean(rolling_data_set)
+            # check if rolling average has a negative spike
+            if diff < 0 and abs(diff / numpy.mean(rolling_data_set)) > anomaly_threshold_percent / 100:
+                # Send Telegram message based Rolling average
+                msg = f'Current funding rate: {data["indicativeFundingRate"]}\nMean funding rate: {numpy.mean(rolling_data_set)}\n' \
+                      f'Spike %: {abs(diff / numpy.mean(rolling_data_set)) * 100} below average\n<b>{bullish_bias_msg}</b> '
+                if messaging == 1:
+                    send_telegram(f'{telegram_token}', telegram_chat_id, msg)
+                    messaging = 0
+            # check if rolling average has a positive spike
+            elif diff > 0 and abs(diff / numpy.mean(rolling_data_set)) > anomaly_threshold_percent / 100:
+                # Send Telegram message based Rolling average
+                msg = f'Current funding rate: {data["indicativeFundingRate"]}\nMean funding rate: {numpy.mean(rolling_data_set)}\n' \
+                      f'Spike %: {abs(diff / numpy.mean(rolling_data_set)) * 100} above average\n<b>{bearish_bias_msg}</b> '
+                if messaging == 1:
+                    send_telegram(f'{telegram_token}', telegram_chat_id, msg)
+                    messaging = 0
+            else:
+                # Rolling data set. Discarding the oldest value and adding new value.
+                rolling_data_set = rolling_data_set[1:]
+                rolling_data_set.append(data['indicativeFundingRate'])
+                messaging = 1
+        # print(rolling_data_set)
+        # sleep(2)
 
 
 if __name__ == '__main__':
-    websocket_trigger('ETH', 'ETHUSD', 21)
-
-
+    bitmex_funding_rate_trigger('ETHUSD', 10000, 500, 'Telegram BOT Key', 'Chat ID',
+                                'Bias: Bearish, please avoid longs', 'Bias: Bullish, please avoid shorts')
